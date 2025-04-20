e247a614c0c4dc3d2dbeeedd5437570c3a13f91a
kokke33 <42080786+kokke33@users.noreply.github.com>
2025-04-20 08:44:22 +0000

    Restored to '7ecd773a176116ae3dbf7875e209e6aee9c24bc0'
    Replit-Restored-To: 7ecd773a176116ae3dbf7875e209e6aee9c24bc0

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
-
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true
   },
-  plugins: [
-    vue(),
-    wasm(),
-    topLevelAwait()
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
